ca1666ac173404b289ea67a6f9527627c4d24dc6
family-monitoring-screen <fauzifauzi64756@gmail.com>
2026-08-03 20:59:21 +0700

    Create ProtectedRoute.tsx

---
+++ src/components/common/ProtectedRoute.tsx
@@ -0,0 +1,22 @@
+import { Navigate, useLocation } from 'react-router-dom'
+import { useAuthStore } from '@/store/authStore'
+import LoadingScreen from './LoadingScreen'
+
+interface ProtectedRouteProps {
+  children: React.ReactNode
+}
+
+export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+  const { isAuthenticated, isLoading } = useAuthStore()
+  const location = useLocation()
+
+  if (isLoading) {
+    return <LoadingScreen />
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" state={{ from: location }} replace />
+  }
+
+  return <>{children}</>
+}
